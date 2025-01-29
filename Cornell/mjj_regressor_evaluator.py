3900801947746f3bfabc6524b5d94cb5aba6bf1f
Jason Fan <jasonfan393@gmail.com>
2025-01-29 04:10:11 +0100

    fix bug with savedmodel

--- Cornell/mjj_regressor_evaluator.py
+++ Cornell/mjj_regressor_evaluator.py
@@ -1,9 +1,10 @@
+from tensorflow.keras.models import load_model
 import pandas as pd
 import awkward as ak
-#import tensorflow as tf
-#from tensorflow.keras.layers.experimental.preprocessing import Normalization
-#from sklearn import preprocessing
-#from tensorflow.keras.optimizers import Adam
+# import tensorflow as tf
+# from tensorflow.keras.layers.experimental.preprocessing import Normalization
+# from sklearn import preprocessing
+# from tensorflow.keras.optimizers import Adam
 from matplotlib import pyplot as plt
 import numpy as np
 import vector
@@ -12,14 +13,15 @@ import json
 import argparse
 import utils
 import os
-#outer-most directory containing merged parquets to apply mjj on
-#FIXME change this to use json with file locations
+# outer-most directory containing merged parquets to apply mjj on
+# FIXME change this to use json with file locations
 preamble = '/eos/user/e/evourlio/HiggsDNA'
 
-parser = argparse.ArgumentParser(prog = 'ProgramName', description = 'placeholder', epilog = 'placeholder')
-parser.add_argument('--vars',default = 'configs/variables_Cornell_mjj.json')
-parser.add_argument('--output_location', default = 'mjj_regressor_output')
-
+parser = argparse.ArgumentParser(
+    prog='ProgramName', description='placeholder', epilog='placeholder')
+parser.add_argument('--vars', default='configs/variables_Cornell_mjj.json')
+parser.add_argument('--output_location', default='mjj_regressor_output')
+parser.add_argument('--attach_inputs', default = True)
 args = parser.parse_args()
 output = args.output_location
 if not os.path.exists(output):
@@ -29,35 +31,46 @@ with open(args.vars, 'r') as file:
     data = json.load(file)
 input_vars = data["input_variables"]
 
-print("Please verify regressor is trained with selected input variables: \n")
+print("Please verify regressor is trained with selected input variables: ")
 for input_var in input_vars:
-  print(input_var + "\n")
+    print(input_var)
 print("# of variables: "+str(len(input_vars)))
 
 file_paths = []
 for subdir, dirs, files in os.walk(preamble):
     for file in files:
         if "merged.parquet" not in file:
-          continue
-        file_path=os.path.join(subdir, file)
+            continue
+        file_path = os.path.join(subdir, file)
         file_paths.append(file_path)
 
-from tensorflow.keras.models import load_model
-#load your Mjj regressor here
+# load your Mjj regressor here
 model = load_model("Mjj_regressor_model")
 
 # main evaluator loop
 
 for file_path in file_paths:
-  print("processing file: " + file_path)
-  df_all = utils.load_parquet_file(file_path, loadAll = True) #load parquet with all variables
-  df_all = utils.add_PNetCorrections(df_all)
-  df_input, extravars = utils.mjj_input_df(df_all, input_vars)
-  df_all["mjj_regressed"] = model.predict(df_input)
+    if "HH" not in file_path:
+        continue #FIXME remov
+    print("processing file: " + file_path)
 
-  destination_path = output + file_path.replace(preamble,"")
-  destination_dir = destination_path.replace("NOTAG_merged.parquet","") #FIXME this is a hack
-  if not os.path.exists(destination_dir):
-      os.makedirs(destination_dir)
-  df_all.to_parquet(destination_path)
-  print("output saved in: " + destination_path)
+    # load parquet with all variables
+    df_all = utils.load_parquet_file(file_path, loadAll=True)
+
+    #add PNET variables (unnecessary and to be removed in future)
+    df_all = utils.add_PNetCorrections(df_all)
+
+    df_input, extravars = utils.mjj_input_df(df_all, input_vars)
+    if args.attach_inputs:
+        df_all = pd.concat([df_all,extravars],axis=1)
+
+    df_all["mjj_regressed"] = model.predict(df_input)
+
+    destination_path = output + file_path.replace(preamble, "")
+    destination_dir = destination_path.replace(
+        "NOTAG_merged.parquet", "")  # FIXME this is a hack
+    if not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
+    df_all.to_parquet(destination_path)
+    print("output saved in: " + destination_path)
+
--- Cornell/utils.py
+++ Cornell/utils.py
@@ -1,33 +1,43 @@
-#file contains some common utility functions for mjj regression tasks
+# file contains some common utility functions for mjj regression tasks
 import pandas as pd
 import vector
 import numpy as np
-def load_parquet_file(file_path,columns = [],loadAll = False):
+
+
+def load_parquet_file(file_path, columns=[], loadAll=False):
     if loadAll:
         df = pd.read_parquet(file_path)
     else:
-        df = pd.read_parquet(file_path, columns = columns)
+        df = pd.read_parquet(file_path, columns=columns)
     df = df.fillna(0)
     return df
 
+
 def add_PNetCorrections(df):
-    #function adds PNetCorrections, will not be necessary as this will be added to the main parquets.
-    PNetCorr_lead_bjet_pt = df['nonRes_lead_bjet_pt']*(1-df['nonRes_lead_bjet_rawFactor'])*df['nonRes_lead_bjet_PNetRegPtRawCorr']*df['nonRes_lead_bjet_PNetRegPtRawCorrNeutrino']
-    PNetCorr_sublead_bjet_pt = df['nonRes_sublead_bjet_pt']*(1-df['nonRes_sublead_bjet_rawFactor'])*df['nonRes_sublead_bjet_PNetRegPtRawCorr']*df['nonRes_sublead_bjet_PNetRegPtRawCorrNeutrino']
-    PNetCorr_lead_bjet_mass = df['nonRes_lead_bjet_mass']*(1-df['nonRes_lead_bjet_rawFactor'])*df['nonRes_lead_bjet_PNetRegPtRawCorr']*df['nonRes_lead_bjet_PNetRegPtRawCorrNeutrino']
-    PNetCorr_sublead_bjet_mass = df['nonRes_sublead_bjet_mass']*(1-df['nonRes_sublead_bjet_rawFactor'])*df['nonRes_sublead_bjet_PNetRegPtRawCorr']*df['nonRes_sublead_bjet_PNetRegPtRawCorrNeutrino']
+    # function adds PNetCorrections, will not be necessary as this will be added to the main parquets.
+    PNetCorr_lead_bjet_pt = df['nonRes_lead_bjet_pt']*(1-df['nonRes_lead_bjet_rawFactor']) * \
+        df['nonRes_lead_bjet_PNetRegPtRawCorr'] * \
+        df['nonRes_lead_bjet_PNetRegPtRawCorrNeutrino']
+    PNetCorr_sublead_bjet_pt = df['nonRes_sublead_bjet_pt']*(1-df['nonRes_sublead_bjet_rawFactor']) * \
+        df['nonRes_sublead_bjet_PNetRegPtRawCorr'] * \
+        df['nonRes_sublead_bjet_PNetRegPtRawCorrNeutrino']
+    PNetCorr_lead_bjet_mass = df['nonRes_lead_bjet_mass']*(1-df['nonRes_lead_bjet_rawFactor']) * \
+        df['nonRes_lead_bjet_PNetRegPtRawCorr'] * \
+        df['nonRes_lead_bjet_PNetRegPtRawCorrNeutrino']
+    PNetCorr_sublead_bjet_mass = df['nonRes_sublead_bjet_mass']*(
+        1-df['nonRes_sublead_bjet_rawFactor'])*df['nonRes_sublead_bjet_PNetRegPtRawCorr']*df['nonRes_sublead_bjet_PNetRegPtRawCorrNeutrino']
 
     jet1 = vector.array({
-        "pt"   : PNetCorr_lead_bjet_pt,
-        "phi"  : df["nonRes_sublead_bjet_phi"],
-        "eta"  : df["nonRes_sublead_bjet_eta"],
-        "mass" : PNetCorr_lead_bjet_mass
+        "pt": PNetCorr_lead_bjet_pt,
+        "phi": df["nonRes_sublead_bjet_phi"],
+        "eta": df["nonRes_sublead_bjet_eta"],
+        "mass": PNetCorr_lead_bjet_mass
     })
     jet2 = vector.array({
-        "pt"   : PNetCorr_sublead_bjet_pt,
-        "phi"  : df["nonRes_lead_bjet_phi"],
-        "eta"  : df["nonRes_lead_bjet_eta"],
-        "mass" : PNetCorr_sublead_bjet_mass
+        "pt": PNetCorr_sublead_bjet_pt,
+        "phi": df["nonRes_lead_bjet_phi"],
+        "eta": df["nonRes_lead_bjet_eta"],
+        "mass": PNetCorr_sublead_bjet_mass
     })
     dijet = jet1 + jet2
     PNet_dijet_corr_mass = dijet["mass"]
@@ -48,12 +58,15 @@ def add_PNetCorrections(df):
         "phi": df['puppiMET_phi']
     })
     corr_MET = MET_Pt - (lead_delta + sublead_delta)
-    #correct sumET
-    lead_delta_Et = np.sqrt(PNetCorr_lead_bjet_pt**2 + PNetCorr_lead_bjet_mass**2) - np.sqrt(df['nonRes_lead_bjet_pt']**2 + df['nonRes_lead_bjet_mass']**2)
-    sublead_delta_Et = np.sqrt(PNetCorr_sublead_bjet_pt**2 + PNetCorr_sublead_bjet_mass**2) - np.sqrt(df['nonRes_sublead_bjet_pt']**2 + df['nonRes_sublead_bjet_mass']**2)
+    # correct sumET
+    lead_delta_Et = np.sqrt(PNetCorr_lead_bjet_pt**2 + PNetCorr_lead_bjet_mass**2) - \
+        np.sqrt(df['nonRes_lead_bjet_pt']**2 + df['nonRes_lead_bjet_mass']**2)
+    sublead_delta_Et = np.sqrt(PNetCorr_sublead_bjet_pt**2 + PNetCorr_sublead_bjet_mass**2) - \
+        np.sqrt(df['nonRes_sublead_bjet_pt']**2 +
+                df['nonRes_sublead_bjet_mass']**2)
     corr_MET_sumET = df['puppiMET_sumEt'] - (lead_delta_Et + sublead_delta_Et)
 
-    #add columns to df
+    # add columns to df
     df["nonRes_lead_bjet_PNetCorr_pt"] = PNetCorr_lead_bjet_pt
     df["nonRes_sublead_bjet_PNetCorr_pt"] = PNetCorr_sublead_bjet_pt
     df["nonRes_lead_bjet_PNetCorr_mass"] = PNetCorr_lead_bjet_mass
@@ -63,30 +76,35 @@ def add_PNetCorrections(df):
     df["nonRes_corr_MET_SumET"] = corr_MET_sumET
     df["nonRes_dijet_corr_mass"] = PNet_dijet_corr_mass
     return df
-def calc_var(df_in,input_var):
+
+
+def calc_var(df_in, input_var):
     if input_var in df_in.columns:
-        raiseException("Should not be using this function for variable already in df")
+        raiseException(
+            "Should not be using this function for variable already in df")
     elif "over" in input_var:
         if "mjj" in input_var:
-            base_var = input_var.replace("_over_mjj","")
+            base_var = input_var.replace("_over_mjj", "")
             return df_in[base_var]/df_in["nonRes_dijet_corr_mass"]
     elif "projectphi" in input_var:
         num = input_var.split("projectphi")[1][0]
         return np.cos(df_in["nonRes_DeltaPhi_j" + str(num) + "MET"])
     elif "eta_sign" in input_var:
-        base_var = input_var.replace("_sign","")
+        base_var = input_var.replace("_sign", "")
         return df_in[base_var] * np.sign(df_in["nonRes_lead_bjet_eta"])
     else:
         print("variable not yet supported")
         return None
 
-def mjj_input_df(df_in,input_vars):
+
+def mjj_input_df(df_in, input_vars):
 
     df_out = pd.DataFrame([])
-    extravars = []
+    extravars = pd.DataFrame([])
     for input_var in input_vars:
         if input_var in df_in.columns:
             df_out[input_var] = df_in[input_var]
         else:
-            df_out[input_var] = calc_var(df_in,input_var)
+            df_out[input_var] = calc_var(df_in, input_var)
+            extravars[input_var] = calc_var(df_in, input_var)
     return df_out, extravars
